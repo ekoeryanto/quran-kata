88377575100e4b5a64623064618943c04ad0062f
ekoeryanto <ekoeryanto@gmail.com>
2020-05-23 05:16:27 +0700

    chore: readability and serve audio/

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,64 @@
+const path = require('path')
+const serveStatic = require('serve-static')
+
 const colors = require('vuetify/es5/util/colors').default
+const locales = {
+  id: require('vuetify/es5/locale/id').default,
+  en: require('vuetify/es5/locale/en').default
+}
+
+const serverMiddleware = []
+
+if (process.env.NODE_ENV !== 'production') {
+  serverMiddleware.unshift({
+    path: '/audio',
+    handler: serveStatic(path.join(__dirname, 'audio'))
+  })
+}
 
 module.exports = {
   mode: 'universal',
+  serverMiddleware,
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify'
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
@@ -50,21 +67,20 @@ module.exports = {
     '@nuxtjs/dotenv'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     defaultAssets: false,
     icons: { iconfont: 'mdiSvg' },
     lang: {
-      locales: { id: require('vuetify/es5/locale/id'), en: require('vuetify/es5/locale/en') },
+      locales,
       current: 'id'
     },
     theme: {
@@ -83,13 +99,12 @@ module.exports = {
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend (config, ctx) {}
   }
 }
